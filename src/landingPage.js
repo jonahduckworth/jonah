49ca4c0de9155c8e7f903e2788163a6eb4262e5b
jones <35585020+fleeced@users.noreply.github.com>
2022-05-24 12:07:18 -0700

    added a cool button to landing page

--- src/landingPage.js
+++ src/landingPage.js
@@ -5,33 +5,35 @@ import 'aos/dist/aos.css';
 import me from './jonah.jpeg';
 
 import './landingPage.css';
-
-//const FadeLeft = styled.div`animation: .5s ${keyframes`${fadeInLeft}`}`;
-//const FadeRight = styled.div`animation: 1s ${keyframes`${fadeInRight}`}`;
+import './enterButton.css';
 
 function MyLandingPage() {
 
     // initialize AOS
     useEffect(() => {
-        AOS.init({ duration:2000 });
+        AOS.init({ duration:1800 });
     }, []);
 
     return (
 
-        <div class="container">  
+        <div class='container'>  
 
             <div className='photo' data-aos='fade-right' data-aos-delay='200'>
                     <img src={me} className="photo" alt="me" />
             </div>
 
-            <div className="content" data-aos='fade-left' data-aos-delay='1500'>
-                    <h3 class="title">hi, i'm jonah</h3>
+            <div className='content' data-aos='fade-left' data-aos-delay='1200'>
+                    <h3 class='title'>hi, i'm jonah</h3>
             </div>
 
-            <div className="content" data-aos='fade-left' data-aos-delay='3000'>
+            <div className='content' data-aos='fade-left' data-aos-delay='2200'>
                     <p>thanks for visiting my website</p>
             </div>
 
+            <div class='enter' data-aos='fade-up' data-aos-delay='3000'>
+                <a href='#' class='btn btn-white btn-animate'>click to enter</a>
+            </div>
+            
         </div>
         
     )
