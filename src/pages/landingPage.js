e5794d856597af08b2130f46c5bc4829eda1f270
jones <35585020+fleeced@users.noreply.github.com>
2022-07-23 11:09:34 -0600

    created containers for photo and content

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+// import { experimentalStyled } from "@material-ui/core";
 
 import me from '../images/jonah.jpeg';
 
 import './landingPage.css';
 import '../utils/enterButton.css';
+
+// const MainLayoutRoot = experimentalStyled("div")(({ theme }) => ({
+//     backgroundColor: "#cbe6ff",
+//     display: "flex",
+//     height: "100%",
+//     overflow: "hidden",
+//     width: "100%",
+// }));
 
 function MyLandingPage() {
 
@@ -16,25 +25,30 @@ function MyLandingPage() {
 
     return (
 
-        <div class='container'>  
 
-            <div className='photo' data-aos='fade-right' data-aos-delay='200'>
-                    <img src={me} className="photo" alt="me" />
-            </div>
-
-            <div className='content' data-aos='fade-left' data-aos-delay='1200'>
-                    <h3 class='title'>hi, i'm jonah</h3>
-            </div>
-
-            <div className='content' data-aos='fade-left' data-aos-delay='2200'>
-                    <p>thanks for visiting my website</p>
-            </div>
-
-            <div class='enter' data-aos='fade-up' data-aos-delay='3000'>
-                <a href='#' class='btn btn-white btn-animate'>click to enter</a>
+        <>
+            <div class='photo-container'>
+                <div data-aos='fade-right' data-aos-delay='200'>
+                    <img src={me} alt="me" />
+                </div>
             </div>
             
-        </div>
+            <div class='content-container'>
+
+                <div data-aos='fade-left' data-aos-delay='1200'>
+                    <h3>hi, i'm jonah</h3>
+                </div>
+
+                <div data-aos='fade-left' data-aos-delay='2200'>
+                    <p>thanks for visiting my website</p>
+                </div>
+
+                <div data-aos='fade-up' data-aos-delay='3000'>
+                    <a href='#' class='btn btn-white btn-animate'>click to enter</a>
+                </div>
+
+            </div>
+        </>
         
     )
 }
